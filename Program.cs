129c4e6dfece15e3cdbadf631c3c46b00001dc9a
Joao Nacur <luisoujpof@gmail.com>
2025-05-16 10:24:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string frase = Console.ReadLine();
+string x = Console.ReadLine();
+string y = Console.ReadLine();
+string z = Console.ReadLine();
+
+// split
+string[] vet = Console.ReadLine().Split (' ');
+string p1 = vet[0];
+string p2 = vet[1];
+string p3 = vet[2];
+
+Console.WriteLine("Você disse: ");
+Console.WriteLine(frase);
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+Console.WriteLine(p1);
