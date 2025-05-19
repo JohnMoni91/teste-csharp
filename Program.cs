9cf5d79eec2dff156be6ebcb37859fc5e781c904
Joao Nacur <luisoujpof@gmail.com>
2025-05-19 11:10:03 -0300

    Dia 2

--- Program.cs
+++ Program.cs
@@ -1,17 +1,38 @@
-﻿string frase = Console.ReadLine();
-string x = Console.ReadLine();
-string y = Console.ReadLine();
-string z = Console.ReadLine();
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
-// split
-string[] vet = Console.ReadLine().Split (' ');
-string p1 = vet[0];
-string p2 = vet[1];
-string p3 = vet[2];
+namespace Course { 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entre com o seu nome completo 
+            Console.WriteLine("Entre com seu nome completo: ");
+            string nome = Console.ReadLine();
 
-Console.WriteLine("Você disse: ");
-Console.WriteLine(frase);
-Console.WriteLine(x);
-Console.WriteLine(y);
-Console.WriteLine(z);
-Console.WriteLine(p1);
+            // Quantos quartos tem na sua casa?
+            Console.WriteLine("Quantos quartos tem na sua casa? ");
+            int qrtos = int.Parse(Console.ReadLine());
+
+            // Entre com o preço de um produto
+            Console.WriteLine("Entre com o preço de um produto: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Entre seu último nome, idade e altura (mesma linha):
+            Console.WriteLine("Entre seu último nome, idade e altura: ");
+            string[] vet = Console.ReadLine().Split(' ');
+            string ultimoNome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            // Mostrar resultado   
+            Console.WriteLine(nome);
+            Console.WriteLine(qrtos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
