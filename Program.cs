9d283fdfdfc3dd222735576f5738be04d6fb89ca
Joao Nacur <luisoujpof@gmail.com>
2025-05-21 12:32:31 -0300

    dia 4 +

--- Program.cs
+++ Program.cs
@@ -1,28 +1,41 @@
 ﻿using System;
 using System.Globalization;
 
+
 namespace Course{
     class Program{
-        static void Main(string[] args){
-            int x, y;
+        static void Main(string[] args)
+        {
+            // Preciso fazer um programa q leia um numero inderterminado de dados
+            // contendo cada um, a idade de um individuo
+            // o ultimo dado, q n entrara nos calculos, cotem um valor de idade negativa
+            // calcular e imprimir a media de idade deste grupo
+            // se houver idade negativa, o programa deve parar de ler os dados e imprimir q é impossivel calcular
 
-            string[] vet = Console.ReadLine().Split(' ');
-            x = int.Parse(vet[0]);
-            y = int.Parse(vet[1]);
+            double idade, media;
+            int cont = 0;
 
-            while (x != y)
+            Console.WriteLine("Digite a idade dos individuos: ");
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double soma = 0.0;
+
+            while (idade >= 0)
             {
-                if (x < y)
-                {
-                    Console.WriteLine("Crescente");
-                }
-                else
-                {
-                    Console.WriteLine("Decrescente");
-                }
-                vet = Console.ReadLine().Split(' ');
-                x = int.Parse(vet[0]);
-                y = int.Parse(vet[1]);
+                soma += idade;
+                cont += 1;
+                Console.WriteLine("Digite a idade dos individuos: ");
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            if (cont == 0)
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+            else
+            {
+                media = soma / cont;
+                Console.WriteLine("A media de idade é: " + media.ToString("F2", CultureInfo.InvariantCulture));
+                Console.ReadLine();
             }
         }
     }
