462ef934a21dc5bae2d81bd11c3da49fa3e921b9
Joao Nacur <luisoujpof@gmail.com>
2025-05-27 21:44:18 -0300

    dia 9+

--- Program.cs
+++ Program.cs
@@ -1,37 +1,49 @@
 ﻿using System;
+using System.Formats.Asn1;
 using System.Globalization;
 
-namespace ConsoleApplication2
+namespace Course
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Ler os lados dos triangulos X e Y
+            Console.WriteLine("Digite os lados do triangulo X: ");
+            double xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double x = 10.35784;
-            int y = 32;
-            string z = "Maria";
-            char w = 'F';
 
-            Console.Write("Olá mundo!");
-            Console.WriteLine("Bom dia!");
-            Console.WriteLine("Até mais.");
+            Console.WriteLine("Digite os lados do triangulo Y: ");
+            double yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine();
-            Console.WriteLine(x);
-            Console.WriteLine(x.ToString("F2"));
-            Console.WriteLine(x.ToString("F4"));
-            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+            //formula de heron para calcular a area de um triangulo
+            // valor de x
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
 
-            Console.WriteLine();
-            Console.WriteLine("RESULTADO = " + x);
-            Console.WriteLine("O valor do troco é " + x + " reais");
-            Console.WriteLine("O valor do troco é " + x.ToString("F2") + " reais");
+            // valor de y
+            double q = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(q * (q - yA) * (q - yB) * (q - yC));
 
-            Console.WriteLine();
-            Console.WriteLine("A paciente " + z + " tem " + y + " anos e seu sexo é: " + w);
 
-            Console.ReadLine();
+            // Exibir as areas
+            Console.WriteLine($"Area X = {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Area Y = {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
+
+            // Verificar qual area é maior
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior area: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior area: Y");
+
+            }
         }
     }
 }
