d9a15accf410434d3baf532df3d3277469d3bb82
Joao Nacur <luisoujpof@gmail.com>
2025-05-29 21:51:07 -0300

    dia 11

---
+++ Produto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Produtos
+{
+    internal class Produto
+    {
+
+        // Get é um método que retorna o valor de um campo ou propriedade.
+        // Set é um método que define o valor de um campo ou propriedade.
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, double preco)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = _quantidade;
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+            else
+            {
+                throw new ArgumentException("Nome inválido. Deve ter mais de um caractere.");
+            }
+
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,44 +1,20 @@
-﻿using System;
+﻿using Produtos;
+using System;
 using System.Globalization;
 
-namespace PrimeiroProjeto
+namespace Course
 {
-    internal class Program
+    class Program
     {
-        // Fazer um programa para ler os dados de um produto em estoque (nome, preço e quantidade no estoque). Em seguida, mostrar o valor total do estoque (preço * quantidade) e permitir que o usuário informe quantos produtos serão adicionados ou removidos do estoque. Ao final, mostrar novamente o valor total do estoque.
-
-        // puxar class do ProdutoEstoque.cs
-
         static void Main(string[] args)
         {
-            // Declarando a outra classe
-            ProdutoEstoque produto = new ProdutoEstoque();
+            Produto p = new Produto("TV", 500.00, 10);
 
-            // Lendo os dados do produto
-            Console.Write("Digite o nome do produto: ");
-            produto.nome = Console.ReadLine() ?? "";
+            p.SetNome("TV 4K");
 
-            //  Preço
-            Console.Write("Digite o preço do produto: ");
-            produto.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            // Quantidade no estoque
-            Console.Write("Digite a quantidade no estoque: ");
-            produto.quantidade = int.Parse(Console.ReadLine() ?? "0");
-            Console.WriteLine($"Valor total do estoque: R$ {produto.ValorTotalEstoque():F2}");
-
-            // Adicionar e remover produtos do estoque
-            Console.Write("Digite a quantidade de produtos a serem adicionados ao estoque: ");
-            int quantidadeAdicionar = int.Parse(Console.ReadLine() ?? "0");
-            produto.AdicionarProdutos(quantidadeAdicionar);
-            Console.WriteLine($"Valor total do estoque após adição: R$ {produto.ValorTotalEstoque():F2}");
-            Console.Write("Digite a quantidade de produtos a serem removidos do estoque: ");
-            int quantidadeRemover = int.Parse(Console.ReadLine() ?? "0");
-            produto.RemoverProdutos(quantidadeRemover);
-            Console.WriteLine($"Valor total do estoque após remoção: R$ {produto.ValorTotalEstoque():F2}");
-
-            Console.WriteLine(produto);
-            Console.ReadLine();
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(p.GetQuantidade());
         }
     }
 }
