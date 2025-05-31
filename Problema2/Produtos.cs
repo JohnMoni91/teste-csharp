ad67994a83b2288e97fe9322e0377a03e0a236f8
Joao Nacur <luisoujpof@gmail.com>
2025-05-31 15:52:09 -0300

    dia 13

---
+++ Problema2/Produtos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema2
+{
+    internal class Produtos
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public Produtos(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+        public override string ToString()
+        {
+            return $"{Nome}, {Preco:C}";
+        }
+        public static double CalcularPrecoTotal(Produtos[] produtos)
+        {
+            return produtos.Sum(p => p.Preco);
+        }
+        public static double CalcularMediaPreco(Produtos[] produtos)
+        {
+            return produtos.Average(p => p.Preco);
+        }
+    }
+}
---
+++ Problema2/Program.cs
@@ -0,0 +1,35 @@
+﻿using Problema2;
+using System.Globalization;
+using System;
+
+namespace Problemas
+    {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ler numero inteiro N e os dados (nome e preço) de N produtos
+            int n = int.Parse(Console.ReadLine());
+            
+            Produtos[] produtos = new Produtos[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                produtos[i] = new Produtos(nome, preco);
+            }
+            // verificar se a quantidade de produtos é maior que zero antes de calcular
+            if (produtos.Length > 0)
+            {
+                // calcular o preço
+                double precoTotal = Produtos.CalcularPrecoTotal(produtos);
+                Console.WriteLine($"Preço Total: {precoTotal:C}");
+
+                // calcular a média
+                double mediaPreco = Produtos.CalcularMediaPreco(produtos);
+                Console.WriteLine($"Média de Preço: {mediaPreco:C}");
+            }
+        }
+    }
+};
